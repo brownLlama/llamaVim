86270d1daefcaa695eb19178c421a4b56d6882be
Sanu Maharjan <brownLlama>
2023-06-08 11:14:15 +0200

    all updated

--- lua/brownllama/plugins-setup.lua
+++ lua/brownllama/plugins-setup.lua
@@ -77,6 +77,7 @@ return packer.startup(function(use)
     -- configuring lsp servers
     use("neovim/nvim-lspconfig") -- easily configure language servers
     use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+    use({ "glepnir/lspsaga.nvim", branch = "main" }) -- advanced UI for LSP
     use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
     use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
     use({
