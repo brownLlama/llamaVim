1eb94d5dfa7ce0f3a3855202a98b6fd5110eeeab
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 15:29:34 +0200

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -47,16 +47,11 @@ echo "plugins=(zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 echo "alias nvim='sudo -E /home/linuxbrew/.linuxbrew/bin/nvim'" >> ~/.zshrc
 echo "alias c=clear" >> ~/.zshrc
 echo "alias rm=trash" >> ~/.zshrc
+echo "function take {
+  mkdir -p $1
+  cd $1
+}" >> ~/.zshrc
 
 source ~/.zshrc
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/sanu_maharjan/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-
-###########################################################################################################################
-
-# Add following on .zshrc
-function take {
-  mkdir -p $1
-  cd $1
-}
