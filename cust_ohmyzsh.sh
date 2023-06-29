7cdc1141cfb39553c2477ef8667aa20dd616c179
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 17:20:38 +0200

    Update cust_ohmyzsh.sh

--- cust_ohmyzsh.sh
+++ cust_ohmyzsh.sh
@@ -1,22 +1,3 @@
-# Insatlling autosuggestions and syntax highlight
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Need to be added with plugins=(git ....)
-echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
-
-# Sudo for nvim
-echo "alias nvim='sudo -E /home/linuxbrew/.linuxbrew/bin/nvim'" >> ~/.zshrc
-echo "alias c=clear" >> ~/.zshrc
-
-# For MacOS
-# echo "alias rm=trash" >> ~/.zshrc
-# echo "function take {
-#   mkdir -p $1
-#   cd $1
-# }" >> ~/.zshrc
-
-source ~/.zshrc
-
-cd
-rm prep_neovim.sh prep_ohmyzsh.sh
+sudo apt update
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
