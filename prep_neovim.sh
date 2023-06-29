a9feec3ad801d35a7ff3154a6cb1f32ad957573b
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 17:22:48 +0200

    Update prep_neovim.sh

--- prep_neovim.sh
+++ prep_neovim.sh
@@ -1,3 +1,25 @@
+# Insatlling autosuggestions and syntax highlight
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Need to be added with plugins=(git ....)
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+
+# Sudo for nvim
+echo "alias nvim='sudo -E /home/linuxbrew/.linuxbrew/bin/nvim'" >> ~/.zshrc
+echo "alias c=clear" >> ~/.zshrc
+
+# For MacOS
+# echo "alias rm=trash" >> ~/.zshrc
+# echo "function take {
+#   mkdir -p $1
+#   cd $1
+# }" >> ~/.zshrc
+
+source ~/.zshrc
+
+###########################################################################################################################
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Change snau_maharjan to your user_name
@@ -23,5 +45,5 @@ mkdir nvim
 cd nvim
 git clone https://github.com/brownLlama/llamaVim.git
 mv llamaVim/* ./
-rm -rf llamaVim README.md prep.sh
+rm -rf llamaVim README.md prep_neovim.sh prep_ohmyzsh.sh
 cd
