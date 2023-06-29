1471d2089651101706672557489c1b107f5694f1
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 22:10:35 +0200

    Update and rename prep_neovim.sh to neovim.sh

--- neovim.sh
+++ neovim.sh
@@ -1,50 +1,35 @@
-# Insatlling autosuggestions and syntax highlight
+# Insatlling autosuggestions and syntax highlight for zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Need to be added with plugins=(git ....)
-echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
-echo "alias c=clear" >> ~/.zshrc
-
-# For MacOS
-# echo "alias rm=trash" >> ~/.zshrc
-# echo "function take {
-#   mkdir -p $1
-#   cd $1
-# }" >> ~/.zshrc
-
-source ~/.zshrc
-
-###########################################################################################################################
-
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# Change snau_maharjan to your user_name
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/sanu_maharjan/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-# Homebrew essentials
 sudo apt-get install build-essential
 brew install gcc
 
 # Install neovim
 brew install neovim
 
-# Install git 
-brew install git
-
-# Install npm
-brew install node
-
-# Make required Directories and download settings for neovim
+# Download and config llamaVim
 mkdir .config
 cd .config
 mkdir nvim
 cd nvim
 git clone https://github.com/brownLlama/llamaVim.git
 mv llamaVim/* ./
-rm -rf llamaVim README.md prep_neovim.sh prep_ohmyzsh.sh
+rm -rf llamaVim README.md neovim.sh ohmyzsh.sh
 cd
 
+# Alias
 # Sudo for nvim
 echo "alias nvim='sudo -E /home/linuxbrew/.linuxbrew/bin/nvim'" >> ~/.zshrc
+# Need to be added with plugins=(git ....)
+echo "plugins=(zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+echo "alias c=clear" >> ~/.zshrc
+echo "function take {
+  mkdir -p $1
+  cd $1
+}" >> ~/.zshrc
 source ~/.zshrc
