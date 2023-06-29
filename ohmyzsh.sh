38a811edd6149938c4bb40950ef3020a421198c9
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 23:14:13 +0200

    Update ohmyzsh.sh

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install unzip npm python3-venv zsh
+sudo apt install unzip python3-venv zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
