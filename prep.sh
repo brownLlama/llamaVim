678c50e8bd8d6d25de41258d737eae43e0ef8090
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 15:24:30 +0200

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -41,6 +41,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Insatlling autosuggestions and syntax highlight
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Need to be added with plugins=(git ....)
 echo "plugins=(zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 
 # Sudo for nvim
