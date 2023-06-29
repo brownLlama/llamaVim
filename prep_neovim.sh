f600a6386844407cf5854e0595aa34bb0d9d5e49
Sanu Maharjan <inbox.sanu@gmail.com>
2023-06-29 16:59:22 +0200

    Update and rename prep.sh to prep_neovim.sh

---
+++ prep_neovim.sh
@@ -0,0 +1,30 @@
+# apt Update
+sudo apt update
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Change snau_maharjan to your user_name
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/sanu_maharjan/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# Homebrew essentials
+sudo apt-get install build-essential
+brew install gcc
+
+# Install neovim
+brew install neovim
+
+# Install git 
+brew install git
+
+# Install npm
+brew install node
+
+# Make required Directories and download settings for neovim
+mkdir .config
+cd .config
+mkdir nvim
+cd nvim
+git clone https://github.com/brownLlama/llamaVim.git
+mv llamaVim/* ./
+rm -rf llamaVim README.md prep.sh
+cd
